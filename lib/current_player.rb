87c929227f3bca077096fe8362fe3b6296917ba4
Armando <salixonline@gmail.com>
2017-10-13 04:57:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns_played = 0
   board.each { |position|
-    (position == "X" || position == "O") ? turns_played += 1 : return
+    (position == "X" || position == "O") ? turns_played += 1 : continue
   }
   turns_played
 end
